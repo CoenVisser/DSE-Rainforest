ced64b1ac737608d20d0b4e61253452def6bd8eb
Elia Mergaert <eliamergaert@gmail.com>
2025-06-03 09:54:44 +0200

    moment calculation

--- #Hooking Mechanism Force Plot.py
+++ #Hooking Mechanism Force Plot.py
@@ -32,19 +32,40 @@ sigma_max = 32*F_asp*l*d/(np.pi*d**4)
 E_tot = (1-v_m**2)/E_m + (1-v_asp**2)/Ex_asp
 f_max = (np.pi*sigma_max/(1-2*v_asp))**3 * 9*R_tip**2/(2*E_tot**2)
 
-#Plotting
-fig, ax = plt.subplots(subplot_kw={'projection':'polar'})
-theta = np.linspace(-alpha, np.arctan(mu_asp)+0.5*np.pi, num)
-r = [f_max]*num
-ax.fill_between(theta, r, color='green', alpha=0.5)
-theta_upper = [np.arctan(mu_asp)+0.5*np.pi] * num
-r_upper = np.linspace(0, f_max, num)
-ax.plot(theta_upper, r_upper, color='red')
+# #Plotting
+# fig, ax = plt.subplots(subplot_kw={'projection':'polar'})
+# theta = np.linspace(-alpha, np.arctan(mu_asp)+0.5*np.pi, num)
+# r = [f_max]*num
+# ax.fill_between(theta, r, color='green', alpha=0.5)
+# theta_upper = [np.arctan(mu_asp)+0.5*np.pi] * num
+# r_upper = np.linspace(0, f_max, num)
+# ax.plot(theta_upper, r_upper, color='red')
 
-theta_lower = [-alpha] * num
-r_lower = np.linspace(0, f_max, num)
-ax.fill_between(theta_lower, r_lower, color='blue')
+# theta_lower = [-alpha] * num
+# r_lower = np.linspace(0, f_max, num)
+# ax.fill_between(theta_lower, r_lower, color='blue')
 
-ax.grid(True)
-ax.set_title('Force Plot for Hooking Mechanism')
-plt.show()
+# ax.grid(True)
+# ax.set_title('Force Plot for Hooking Mechanism')
+# plt.show()
+
+
+# Geometrical parameters
+
+lh = 0.3
+lb = 0.3
+
+ah = 20
+ab = 20
+
+bh = 10
+bb = 10
+
+x = 0.1
+
+Fs = m*9.81/n
+Fnb = m*9.81*(x + lh*np.cos(np.deg2rad(ah))*np.sin(np.deg2rad(bh)))/(lh*np.cos(np.deg2rad(ah))*np.cos(np.deg2rad(bh)) + lb*np.cos(np.deg2rad(ab))*np.cos(np.deg2rad(bb)))/n
+Fnh = -Fnb
+
+Fmax = np.sqrt(Fs**2 + Fnh**2)
+theta = np.arctan2(Fnb, Fnh)
