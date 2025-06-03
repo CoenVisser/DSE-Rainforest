bd71d51f5a67378f93e645c3a49b15f9e43c38b1
IvenD <imdolman@tudelft.nl>
2025-06-03 16:17:54 +0200

    impact

--- #Hooking Mechanism Force Plot.py
+++ #Hooking Mechanism Force Plot.py
@@ -13,29 +13,33 @@ num = 1000                   #Plotting variable
 #======================================================================
 
 # Hook Geometry
-l_hook = 0.023               # Length of the hooks [m]
-d_hook = 0.254* 10**(-3)              # Diameter of the hooks [m]
-n_hook = 30                # Number of hooks [-]
-R_tip = 20* 10**(-6)        # Radius of curvature of the spine [m]
+l_hook = 0.023                          # Length of the hooks [m]
+d_hook = 0.254* 10**(-3)                # Diameter of the hooks [m]
+n_hook = 30                             # Number of hooks [-]
+R_tip = 20* 10**(-6)                    # Radius of curvature of the spine [m]
 
-# Hook Material
-E_m = 200* 10**9                # Elastic modulus [Pa]
-v_m = 0.29                      # Poisson's ratio [-]
-sigma_yield = 290* 10**6        # Yield strength [Pa]
+# Hook Material - Stainless Steel
+E_hook = 200* 10**9                        # Elastic modulus [Pa]
+v_hook = 0.29                              # Poisson's ratio [-]
+sigma_yield_hook = 290* 10**6                # Yield strength [Pa]
+rho_hook = 7790                                 # Density [kg/m^3]
 
 #=======================================================================
 # Spine Properties
 #=======================================================================
 
 # Spine Geometry
-l_spine = 0.7                   # Length of the spine to cg [m]
-d_spine = 0.01                  # Diameter of the spine [m]
-alpha_spine = 20                # Angle of the spine with respect to the symmetry plane [degrees]
-beta_spine = 10                 # Angle of the spine with respect to the horizontal plane [degrees]
-n_spine = 2                     # Number of spines [-]
+l_spine = 0.7                           # Length of the spine to cg [m]
+d_spine = 0.01                          # Diameter of the spine [m]
+alpha_spine = 20                        # Angle of the spine with respect to the symmetry plane [degrees]
+beta_spine = 10                         # Angle of the spine with respect to the horizontal plane [degrees]
+n_spine = 2                             # Number of spines [-]
 
-# Spine Material
-...
+# Spine Material - Aluminium
+E_spine = 25* 10**9          # Elastic modulus [Pa]
+sigma_yield_spine = 270* 10**6        # Yield strength [Pa]
+rho_spine = 2700                      # Density [kg/m^3]
+
 
 #=======================================================================
 # Bumper Properties
@@ -48,8 +52,10 @@ alpha_bumper = 20               # Angle of the bumper with respect to the symmet
 beta_bumper = 10                # Angle of the bumper with respect to the horizontal plane [degrees]
 n_bumper = 2                    # Number of bumpers [-]
 
-# Bumper Material
-...
+# Bumper Material - EDPM Rubber
+# Include bounciness for impact absorption
+E_bumper = 6 * 10**6                # Elastic modulus [Pa]
+rho_bumper = 1430                   # Density [kg/m^3]
 
 #=======================================================================
 # Centre of Mass Properties
@@ -60,10 +66,11 @@ l_cg = 0.1                      # Distance from the surface to the center of mas
 #=======================================================================
 # Propeller Properties
 #=======================================================================
-c_prop = 0.50                   #Clearance of the propeller [%]
+clong_prop = 0.50                  #Cloning of the propeller [-]
+clat_prop = 0.50                   #Clearance of the propeller [%]
 d_prop = 0.25                   #Diameter of the propeller [m]
-l_prop = np.sqrt(2*(d_prop*(1+c_prop)))                       #Length of the propeller arm for double symmetric quadcopter[m]
-alpha_prop =   np.arctan(d_prop*(1+c_prop), d_prop*(1+c_prop))                 #Angle of the propeller with the symmetry plane [degrees]
+l_prop = np.sqrt((d_prop*(1+clong_prop))**2+(d_prop*(1+clat_prop))**2)                                         #Length of the propeller arm for double symmetric quadcopter[m]
+alpha_prop =   np.arctan(d_prop/2*(1+clat_prop), d_prop/2*(1+clong_prop))                  #Angle of the propeller with the symmetry plane [degrees]
 beta_prop =   0                  #Angle of the propeller with the horizontal plane [degrees]
 n_prop = 4                       #Number of propellers [-]
 
@@ -72,14 +79,15 @@ n_prop = 4                       #Number of propellers [-]
 #========================================================================
 
 alpha = 15                      # Adhesion load angle [degrees]
+dynamic_load_factor = 10          # Dynamic load factor [-]
 
 #=======================================================================
 # Tree Properties
 #=======================================================================
 
-mu_asp = 0.20                   # Coefficient of friction [-]
-Ex_asp = 9.8 *10**9             # Young's modulus [Pa]
-v_asp = 0.4                     # Poisson's ratio [-]
+mu_tree = 0.20                   # Coefficient of friction [-]
+E_tree = 9.8 *10**9             # Young's modulus [Pa]
+v_tree = 0.4                     # Poisson's ratio [-]
 
 #========================================================================
 # Bark Properties
@@ -107,8 +115,25 @@ F_tree = (np.pi*sigma_max/(1-2*v_asp))**3 * 9*R_tip**2/(2*E_tot**2)
 F_mat = np.pi/4 * d_hook**2 * sigma_yield
 F_max = min(F_tree, F_mat)                                  #Minimum sizing force
 
-F_actual = F_tot*10
+F_actual = F_tot*dynamic_load_factor
 
+#=======================================================================
+#Impact Calculations
+#=======================================================================
+
+lift_to_weight_ratio_impact = 1.5
+delta_t_impact = 0.4
+moment_of_intertia = m*(l_bumper*np.cos(np.deg2rad(beta_bumper))*np.sin(np.deg2rad(alpha_bumper)))**2
+
+acceleration_impact = 0.5*(0.5*np.pi)/(delta_t_impact**2)
+omega_impact = (0.5*np.pi)/delta_t_impact
+v_impact = omega_impact * (l_bumper*np.cos(np.deg2rad(beta_bumper))*np.sin(np.deg2rad(alpha_bumper)))
+
+F_impact = m*v_impact/delta_t_impact
+sigma_impact = F_impact/(l_bumper*d_bumper)
+
+
+#=======================================================================
 #Plotting
 #=======================================================================
 
